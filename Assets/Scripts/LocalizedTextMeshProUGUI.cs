4a8695717feb8b25f37c9a8fafddc12718939c15
ZagoIris <59665991+ZagoIris@users.noreply.github.com>
2025-07-26 03:24:11 -0300

    Sistema de localização + Main Menu + Pause + Diário

---
+++ Assets/Scripts/LocalizedTextMeshProUGUI.cs
@@ -0,0 +1,38 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.SimpleLocalization.Scripts
+{
+	/// <summary>
+	/// Localize text component.
+	/// </summary>
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class LocalizedTextMeshProUGUI : MonoBehaviour
+    {
+        public string LocalizationKey;
+        private bool localized = false;
+
+        public void Start()
+        {
+            if (!localized)
+            {
+                Localize();
+                localized = true;
+            }
+            
+            LocalizationManager.OnLocalizationChanged += Localize;
+        }
+
+        public void OnDestroy()
+        {
+            LocalizationManager.OnLocalizationChanged -= Localize;
+        }
+
+        public void Localize()
+        {
+            GetComponent<TextMeshProUGUI>().text = LocalizationManager.Localize(LocalizationKey);
+            localized = true;
+        }
+    }
+}
