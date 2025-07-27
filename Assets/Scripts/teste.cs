2df0821c3096a4805c4767630bce4db4f197be86
ZagoIris <59665991+ZagoIris@users.noreply.github.com>
2025-07-27 12:28:59 -0300

    ajuste global volume

--- Assets/Scripts/teste.cs
+++ Assets/Scripts/teste.cs
@@ -1,11 +1,8 @@
-using Unity.VisualScripting;
-using UnityEditor.UI;
+
 using UnityEngine;
-using UnityEngine.UI;
 
 public class teste : MonoBehaviour
 {
-    SliderEditor sliderEditor;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
